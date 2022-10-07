8c7c206dbdc3ab9e3ad15729a6251d6112c8bdfc
mike <michal.mytych@ftggroup.com>
2022-10-07 20:30:17 +0200

    Rm redundant

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { isUserAuthenticated } from '../features/auth'
 
 export default function Home() {
   return (
     <div>
       <h1>Solvable</h1>
       <h3>Backend for an application for interactive programming learning.</h3>
-      {isUserAuthenticated() ? <h4>You are logged in.</h4> : <Link to={'/login'} />}
     </div>
   )
 }
