3adf1f4bee54f5099df45c7f3193bfed83cc605f
mike <michal.mytych@ftggroup.com>
2022-10-12 00:23:03 +0200

    Added real-time solution execution updates

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,26 @@
+import Pusher from "pusher-js"
+
 export const firstWhere = (items, key, value) => {
   const result = items.filter(item => item[key] === value)
   return result.length ? result[0] : null
 }
+
+export const initChannel = (channelName, onReceived) => {
+  const pusher = new Pusher('d1bdabed3a3f56fe70ec', {
+    cluster: 'eu',
+    encrypted: true
+  })
+  const channel = pusher.subscribe(channelName)
+  channel.bind(
+    'executed-new-solution-test',
+    data => onReceived(data),
+    channel.unbind()
+  )
+}
+
+export const uniqueByKey = (arr, key) => [
+  ...new Map(arr.map(item => [item[key], item])).values()
+];
 
 export const addingIntegersInCpp = `
 #include <iostream>
--- src/pages/Solutions/Commit.js
+++ src/pages/Solutions/Commit.js
@@ -4,18 +4,29 @@ import Textarea from '../../components/atoms/Textarea'
 import Button from '../../components/atoms/Button'
 import { getAll as getAllProblems } from '../../features/problems'
 import { getAll as getAllLanguages } from '../../features/languages'
-import { addingIntegersInCpp } from '../../helpers'
+import { addingIntegersInCpp, initChannel, uniqueByKey } from '../../helpers'
 import { commit } from '../../features/solutions'
-import Steps from '../../components/molecules/Steps'
 
 export default function Commit() {
   const [problems, setProblems] = useState([])
+  const [executions, setExecutions] = useState([])
   const [problem, setProblem] = useState()
   const [languages, setLanguages] = useState([])
   const [language, setLanguage] = useState()
   const [code, setCode] = useState(addingIntegersInCpp)
   const [commited, setCommited] = useState(false)
   const [result, setResult] = useState()
+
+  useEffect(() => {
+    initChannel('solution-tests-executions-updates', (data) => {
+      // Some bug with pusher instance being replicated in memory
+      // leads to initChannel being called 2 times so as long
+      // as it won't be fixed, we should pick only distinct
+      // executions objects from the list
+      setExecutions(executions => uniqueByKey([...executions, data.execution], 'id'))
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     getAllProblems(setProblems)
@@ -39,31 +50,35 @@ export default function Commit() {
     <div>
       <h1>Commit a solution</h1>
       <h5>{problem ? problem.title : 'Select problem'}</h5>
-      <Select 
-        items={problems.data} 
-        _key={'title'} 
+      <Select
+        items={problems.data}
+        _key={'title'}
         onSelect={setProblem}
         disabled={commited}
       />
       <h5>{language ? language.name : 'Select language'}</h5>
-      <Select 
-        items={languages} 
+      <Select
+        items={languages}
         onSelect={setLanguage}
         disabled={commited}
       />
       <h5>Write down your solution</h5>
-      <Textarea 
-        value={code} 
-        onChangeHandler={setCode} 
+      <Textarea
+        value={code}
+        onChangeHandler={setCode}
         disabled={commited}
       />
-      <Button 
-        text='Save' 
-        onClickHandler={() => setCommited(true)} 
+      <Button
+        text='Save'
+        onClickHandler={() => setCommited(true)}
         disabled={commited}
       />
       {result ? <h4>{result.data.message}</h4> : null}
-      <Steps />
+      {
+        executions.map((execution, ix)=> (
+          <div key={ix}>⚫️ {execution.output}</div>
+        ))
+      }
     </div>
   )
 }
